b155c0fb541bd46a34523d69eaa32c29bce8975e
GaoBo <gaobo_1000@163.com>
2018-04-24 22:50:01 +0800

    死死死死死死死死死死死

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+    /**
+     * Failed with error: repository 'https://github.com/qaz8234328/test-1.git/' not found
+     */
+}
